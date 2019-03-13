899ad9b0cce8d616ccb6a6a625e2f69fe60b74ab
Asif Imtiaz Shaafi <shaafi.20994@gmail.com>
2019-03-14 01:49:38 +0600

    dialog layout for landscape fixed. android level copyright added.

--- AppHouseBD_bottomSheet/src/main/java/com/apphousebd/apphousebd_bottomsheet/AppHouseBDBottomSheetDialog.kt
+++ AppHouseBD_bottomSheet/src/main/java/com/apphousebd/apphousebd_bottomsheet/AppHouseBDBottomSheetDialog.kt
@@ -1,3 +1,16 @@
+/*
+ * Copyright (c) 2018-2019 AppHouseBD. All rights reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.apphousebd.apphousebd_bottomsheet
 
 import android.content.Context
@@ -7,11 +20,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
 import android.widget.Toast
 import androidx.browser.customtabs.CustomTabsIntent
 import androidx.fragment.app.FragmentManager
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import kotlinx.android.synthetic.main.apphousebd_bottom_sheet.view.*
+
 
 class AppHouseBDBottomSheetDialog : BottomSheetDialogFragment() {
 
@@ -21,6 +38,13 @@ class AppHouseBDBottomSheetDialog : BottomSheetDialogFragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.apphousebd_bottom_sheet, container, false)
+        view.viewTreeObserver.addOnGlobalLayoutListener {
+            val dialog = dialog as BottomSheetDialog?
+            val bottomSheet = dialog!!.findViewById(R.id.design_bottom_sheet) as FrameLayout?
+            val behavior = BottomSheetBehavior.from(bottomSheet!!)
+            behavior.state = BottomSheetBehavior.STATE_EXPANDED
+            behavior.peekHeight = 0
+        }
 
         view.apphousebd_facebook.setOnClickListener {
             openLink("https://facebook.com/apphousebd")
@@ -48,7 +72,7 @@ class AppHouseBDBottomSheetDialog : BottomSheetDialogFragment() {
 
     }
 
-    fun openInAppBrowser(context: Context, url: String) {
+    private fun openInAppBrowser(context: Context, url: String) {
 
         val builder = CustomTabsIntent.Builder()
         builder.setShowTitle(true)
